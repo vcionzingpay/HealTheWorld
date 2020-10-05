b84e57d9bef58e80306f07479bde8698e949ef4a
vcionzingpay <lyhungtrong283@gmail.com>
2020-10-05 20:53:47 +0700

    FuckSon

---
+++ app/src/main/java/itec/hcmus/edu/vn/healtheworld/MainActivity.java
@@ -0,0 +1,20 @@
+package itec.hcmus.edu.vn.healtheworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+//    public void Home_Onclick(View view) {
+//        Toast.makeText(view.getContext(),"Introduction")
+//    }
+}
---
+++ app/src/main/java/itec/hcmus/edu/vn/healtheworld/TheTable.java
@@ -0,0 +1,14 @@
+package itec.hcmus.edu.vn.healtheworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TheTable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_table);
+    }
+}
